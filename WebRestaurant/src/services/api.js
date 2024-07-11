8cad08bbd67938b635e2dc030265fa77cdf72e1e
j4ckson185 <jackson_division@hotmail.com>
2024-07-11 00:54:30 -0300

    Atualizando IP api.js

--- WebRestaurant/src/services/api.js
+++ WebRestaurant/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Strings from "../constants/Strings";
 
-const baseURL = process.env.API_BASE_URL || "http://127.0.0.1";
+const baseURL = process.env.API_BASE_URL || "http://18.222.213.75";
 
 const api = axios.create({
   baseURL: baseURL,
